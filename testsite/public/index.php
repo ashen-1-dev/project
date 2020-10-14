ddc9e9c97781258fdff83408ec8f8bbb9152c08e
ashen-1-dev <alen2014@gmail.com>
2020-10-15 05:25:12 +0700

    v1

---
+++ testsite/public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Nice</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+        
+    }
+</style>
+</head>
+<body>
+<h1> Server is working!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.
+
+
+
+</p>
+
+
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
