32dd4c8628d801ae520f0c2e43aa42f7e4e1f137
ashen-1-dev <alen2014@gmail.com>
2020-11-09 00:40:02 +0700

    -Добавил контроллеры -Написал план реализации API(/notes)

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function (){
+    return 'Success!';
+});
+
+//Route::post('/login', 'LoginController')->name('login_route');
+//Route::post('/register', 'RegisterController')->name('register_route');
+
 Route::get('/lists', 'ListController@allData')->name('list_data'); // Вывод всех списков GET
 Route::post('/lists', 'ListController@createList')->name('list_form'); //  Cоздание нового списка POST
 Route::get('/lists/{list}', 'ListController@showList')->name('list_one_form'); // Показать  список GET
